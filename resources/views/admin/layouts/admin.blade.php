1318e5f780e174fc40db722dc3ef8becd2423301
sumit27x <sumitgupta27x@gmail.com>
2024-05-22 11:00:07 +0530

    change .env

--- resources/views/admin/layouts/admin.blade.php
+++ resources/views/admin/layouts/admin.blade.php
@@ -10,9 +10,11 @@
 
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-    @viteReactRefresh
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
     
@@ -72,5 +74,10 @@
             @yield('content')
         </main>
     </div>
+
+    <!-- script bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
 </body>
 </html>
--- vite.config.js
+++ vite.config.js
@@ -14,3 +14,21 @@ export default defineConfig({
         react(),
     ],
 });
+
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import { resolve } from 'path';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: resolve(__dirname, 'public/build'),
+//     rollupOptions: {
+//       input: {
+//         main: resolve(__dirname, 'resources/js/app.jsx'),
+//       }
+//     }
+//   }
+// });
